458d5569671dd81b55a77305ebb42a9e5fc33381
MR.HO <thinhhoquang@gmail.com>
2013-01-10 21:15:18 +0700

    Add project

---
+++ MAlarmApp/MAlarmApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MAlarmApp
+//
+//  Created by Taishi Yoshida on 1/10/13.
+//  Copyright (c) 2013 Taishi Yoshida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MAlarmApp/MAlarmApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MAlarmApp
+//
+//  Created by Taishi Yoshida on 1/10/13.
+//  Copyright (c) 2013 Taishi Yoshida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
